e4c76d1663b9cb856082d64e91f84e19c6458489
Peter Mahnke <peter.mahnke@canonical.com>
2020-10-15 13:29:55 +0100

    Update webpack.config.entry.js

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "global-nav": "./static/js/global-nav.js",
-  "cookie-policy": "./static/js/cookie-policy.js",
+  ["global-nav"]: "./static/js/global-nav.js",
+  ["cookie-policy"]: "./static/js/cookie-policy.js",
 };
